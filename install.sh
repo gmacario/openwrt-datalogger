53c136e770a286b8aeb8427da8a6e60c127286fc
Gianpaolo Macario <gmacario@gmail.com>
2015-12-08 16:58:15 +0100

    Installed from git

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 #set -x
 set -e
 
+[ -z "${DATALOGGER_URL}" ] && DATALOGGER_URL=https://github.com/gmacario/openwrt-datalogger.git
+[ -z "${DATALOGGER_BRANCH}" ] && DATALOGGER_BRANCH=master
+[ -z "${WORKDIR}" ] && WORKDIR=${HOME}/openwrt-datalogger
+
 die() {
     echo "ERROR: $*"
     exit 1
@@ -11,8 +15,17 @@ die() {
 # opkg update
 # opkg install git nmap wireless-tools
 
-which git2 || die "Please install git"
-which nmap || die "Please install nmap"
-which iwconfig || die "Please install wireless-tools"
+which git >/dev/null || die "Please install git"
+which nmap >/dev/null || die "Please install nmap"
+which iwconfig >/dev/null || die "Please install wireless-tools"
+
+if [ ! -e ${WORKDIR} ]; then
+    echo "INFO: Checking out ${DATALOGGER_URL} into ${WORKDIR}"
+    git clone -b ${DATALOGGER_BRANCH} ${DATALOGGER_URL} ${WORKDIR}
+else
+    echo "INFO: Updating ${WORKDIR}"
+    cd ${WORKDIR}
+    git checkout ${DATALOGGER_BRANCH} && git fetch --all
+fi
 
 # EOF
